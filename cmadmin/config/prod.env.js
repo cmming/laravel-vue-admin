b665d57cc3996328a1adbc68bfe6ca42d10cd7b4
chenming <294225707@qq.com>
2017-10-09 09:49:13 +0800

    修改配置

--- cmadmin/config/prod.env.js
+++ cmadmin/config/prod.env.js
@@ -1,4 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"./api/"'
+  // API_ROOT: '"http://192.168.0.88/laravel/cmapi/public/api/"'
+  API_ROOT: '"http://test.17huang.com/api"'
 }
